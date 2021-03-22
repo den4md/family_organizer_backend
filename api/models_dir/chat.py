fa4f53f3fb549d3b0fc3920b84a3812283bb4782
den4md <denisstarr@gmail.com>
2021-03-22 22:57:58 +0200

    Remove unnecessary field (image) because it will correspond either user avatar or group image

--- api/models_dir/chat.py
+++ api/models_dir/chat.py
@@ -3,6 +3,5 @@ from django.db import models
 
 
 class Chat(models.Model):
-    image = models.ForeignKey(to='File', null=True, on_delete=models.SET_NULL, related_name='+', blank=True)
     user_member_list = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='+', through='ChatUserSettings')
     group = models.ForeignKey(to='Group', on_delete=models.CASCADE, related_name='chat_list')
